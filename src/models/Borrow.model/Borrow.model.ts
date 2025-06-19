fb0bc8700d7dacb968310367cf523c56a819a252
Muhammad Ranju <mdranju23@gmail.com>
2025-06-20 00:43:37 +0600

    fix: update required field validation messages in Borrow model schema

--- src/models/Borrow.model/Borrow.model.ts
+++ src/models/Borrow.model/Borrow.model.ts
@@ -6,16 +6,16 @@ const BorrowSchema = new Schema<IBorrow>(
     book: {
       type: Schema.Types.ObjectId,
       ref: "Book",
-      required: true,
+      required: [true, "Book is required"],
     },
     quantity: {
       type: Number,
-      required: true,
+      required: [true, "Quantity is required"],
       min: [1, "Quantity must be at least 1"],
     },
     dueDate: {
       type: Date,
-      required: true,
+      required: [true, "Due date is required"],
     },
   },
   {
