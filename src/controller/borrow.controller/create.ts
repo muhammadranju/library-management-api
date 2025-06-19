d120713219642183e449f286e6f09cf3aaef3d37
Muhammad Ranju <mdranju23@gmail.com>
2025-06-20 00:43:24 +0600

    feat: implement borrow creation logic with book availability checks

--- src/controller/borrow.controller/create.ts
+++ src/controller/borrow.controller/create.ts
@@ -1,10 +1,53 @@
 import { Request, Response } from "express";
 import Borrow from "../../models/Borrow.model/Borrow.model";
+import Book from "../../models/Book.model/Book.model";
 
 const createBorrow = async (req: Request, res: Response): Promise<void> => {
   try {
     const { book, quantity, dueDate } = req.body;
 
+    const findBook = await Book.findById(book);
+
+    // Handle the case where the book is not found
+    if (!findBook) {
+      res.status(404).json({
+        message: "Borrow creation failed",
+        success: false,
+        error: "Book not found",
+      });
+      return;
+    }
+
+    // Check availability
+    if (findBook.copies === 0 && !findBook.available) {
+      res.status(400).json({
+        message: "Borrow creation failed",
+        success: false,
+        error: "Book not available",
+      });
+      return;
+    }
+
+    // Deduct quantity
+    if (findBook.copies < quantity) {
+      res.status(400).json({
+        message: "Borrow creation failed",
+        success: false,
+        error: `Not enough ${quantity} copies available`,
+      });
+      return;
+    }
+
+    findBook.copies -= quantity;
+
+    // Update availability if copies become zero
+    // if (findBook.copies === 0) {
+    //   findBook.available = false;
+    // }
+
+    await findBook.save();
+
+    // Save borrow record
     const borrow = new Borrow({
       book,
       quantity,
