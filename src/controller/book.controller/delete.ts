c9e377a30ca7071cf2c14466f7c079e99526fe63
Muhammad Ranju <mdranju23@gmail.com>
2025-06-19 22:16:29 +0600

    refactor: enhance deleteBook function with proper error handling and return types

--- src/controller/book.controller/delete.ts
+++ src/controller/book.controller/delete.ts
@@ -1,21 +1,31 @@
 import Book from "../../models/Book.model/Book.model";
 import { Request, Response } from "express";
 
-const deleteBook = async (req: Request, res: Response) => {
+const deleteBook = async (req: Request, res: Response): Promise<void> => {
   try {
     const { id } = req.params;
     const book = await Book.findById(id);
+
     if (!book) {
-      return res.status(404).json({ message: "Book not found" });
+      res
+        .status(404)
+        .json({ message: "Book not found", success: false, data: null });
+      return;
     }
+
     await book.deleteOne();
+
     res.status(200).json({
       success: true,
       message: "Book deleted successfully",
       data: null,
     });
   } catch (error) {
-    res.status(500).json({ error });
+    res.status(500).json({
+      success: false,
+      message: "Book deletion failed",
+      error,
+    });
   }
 };
 
