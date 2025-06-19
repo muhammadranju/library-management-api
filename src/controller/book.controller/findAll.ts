5c68077feefdf5c07486f11228d66d7c8a9b9144
Muhammad Ranju <mdranju23@gmail.com>
2025-06-19 22:16:35 +0600

    fix: correct response status and structure in findAllBooks function

--- src/controller/book.controller/findAll.ts
+++ src/controller/book.controller/findAll.ts
@@ -4,15 +4,19 @@ import { Request, Response } from "express";
 const findAllBooks = async (req: Request, res: Response) => {
   try {
     const books = await Book.find();
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Books retrieved successfully",
-        data: books,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Books retrieved successfully",
+      data: books,
+    });
   } catch (error) {
-    res.status(500).json({ error });
+    res
+      .status(500)
+      .json({
+        message: "Books retrieval failed",
+        success: false,
+        error: error,
+      });
   }
 };
 
