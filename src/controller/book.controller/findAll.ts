fd99c6128d28d41ce3fefd802c3f8b5e165a228a
Muhammad Ranju <mdranju23@gmail.com>
2025-06-20 00:43:11 +0600

    feat: implement filtering, sorting, and pagination in findAllBooks function

--- src/controller/book.controller/findAll.ts
+++ src/controller/book.controller/findAll.ts
@@ -3,20 +3,33 @@ import { Request, Response } from "express";
 
 const findAllBooks = async (req: Request, res: Response) => {
   try {
-    const books = await Book.find();
+    const {
+      filter,
+      sortBy = "createdAt",
+      sort = "desc",
+      limit = 10,
+    } = req.query;
+
+    const query: any = {};
+    if (filter) {
+      query.genre = filter;
+    }
+
+    const books = await Book.find(query)
+      .sort({ [sortBy as string]: sort === "desc" ? -1 : 1 })
+      .limit(parseInt(limit as string));
+
     res.status(200).json({
       success: true,
       message: "Books retrieved successfully",
       data: books,
     });
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        message: "Books retrieval failed",
-        success: false,
-        error: error,
-      });
+    res.status(500).json({
+      message: "Books retrieval failed",
+      success: false,
+      error: error,
+    });
   }
 };
 
